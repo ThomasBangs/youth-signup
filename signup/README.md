a5aac5bc9fb8ab2e750a4af7421e01c4a5516aef
ThomasBangs <BangsSolutions@gmail.com>
2015-05-06 14:17:40 -0500

    added ejs view engine

--- signup/README.md
+++
@@ -1,1 +0,0 @@
-This is a sign up app for youth sports leagues to have online sign up forms that save to a database. It will use Bootstrap, Express, Mongoose and Mongo.
--- signup/app.js
+++ signup/app.js
@@ -4,16 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-var db = require("./db");
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -23,8 +19,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//include instructions on how to connect to database
+var db = require("./db");
+
+//set up route handlers for the root URL
+var routes = require('./routes/index');
 app.use('/', routes);
+
+
+//set up route handler for /user URL 
+var users = require('./routes/users');
 app.use('/users', users);
+ 
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
