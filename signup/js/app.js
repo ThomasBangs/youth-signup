05ba71acd558c5ebc97686e209f4ec7617dbe443
ThomasBangs <BangsSolutions@gmail.com>
2015-06-13 19:27:49 -0500

    added route provider

--- signup/js/app.js
+++ signup/js/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('sportsSignUpApp', []);
+var app = angular.module("sportsSignUpApp", ["ngRoute"]);
+
+//routes for partials
+/*
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/',
+        controller: ''
+      }).
+      when('/', {
+        templateUrl: 'partials/',
+        controller: ''
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
+*/
