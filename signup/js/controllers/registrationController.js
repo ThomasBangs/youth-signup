6aeef395f373e36037972b2e7f3367f1286cef93
ThomasBangs <BangsSolutions@gmail.com>
2015-06-24 18:33:16 -0500

    added ng-click to experience

--- signup/js/controllers/registrationController.js
+++ signup/js/controllers/registrationController.js
@@ -6,6 +6,19 @@ app.controller('registrationController', ['$scope', '$firebaseAuth', '$firebaseA
   $scope.cheerleaderSelect = false;
   $scope.genderMale = false;
   $scope.genderFemale = false;
+
+  //   function($firebaseObject) {
+  //   var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com");
+  //   // download physicsmarie's profile data into a local object
+  //   // all server changes are applied in realtime
+  //   $scope.profile = $firebaseObject(ref.child('profiles').child('phsyicsmarie'));
+  // }
+
+
+   
+
+
+  
 // cheer / football select
   $scope.footballSelected = function() {
     $scope.footballSelect = true;
@@ -34,16 +47,16 @@ app.controller('registrationController', ['$scope', '$firebaseAuth', '$firebaseA
   $scope.saveRegistration = function() {
     // Create JSON document
     var newReg = {
-      "childsName": $scope.childsName,
-      "footballSelect": $scope.footballSelect,
-      "cheerleaderSelect": $scope.cheerleaderSelect,
-      "dateOfBirth": $scope.dateOfBirth,
-      "ageOnJuly31": $scope.ageOnJuly31,
-      "playerStreetAddress": $scope.playerStreetAddress,
-      "playerCity": $scope.playerCity,
-      "zipCode": $scope.zipCode,
-      "genderFemale": $scope.genderFemale,
-      "genderMale": $scope.genderMale,
+      // "childsName": $scope.childsName,
+      // "footballSelect": $scope.footballSelect,
+      // "cheerleaderSelect": $scope.cheerleaderSelect,
+      // "dateOfBirth": $scope.dateOfBirth,
+      // "ageOnJuly31": $scope.ageOnJuly31,
+      // "playerStreetAddress": $scope.playerStreetAddress,
+      // "playerCity": $scope.playerCity,
+      // "zipCode": $scope.zipCode,
+      // "genderFemale": $scope.genderFemale,
+      // "genderMale": $scope.genderMale,
 
 
 
