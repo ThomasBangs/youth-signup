1d7d4412b8fdd5e0ad6a06d83ba422eedbd6108b
ThomasBangs <BangsSolutions@gmail.com>
2015-07-08 16:45:48 -0500

    fixed route

--- signup/js/controllers/profileCtrl.js
+++ signup/js/controllers/profileCtrl.js
@@ -1,9 +1,9 @@
 app.controller("ProfileCtrl",  function($scope, $rootScope, $location, myService, API_URL, $firebaseAuth, Auth, $firebaseObject){
 // connect to firebase
 console.log($rootScope);
-    $scope.myData = new Firebase("https://sportssignup.firebaseio.com/users/" + $rootScope.authData.auth.uid);
+    $scope.myData = new Firebase("https://sportssignup.firebaseio.com/users/" + $rootScope.authData.auth.uid + "/profile");
 
-    $scope.profile = $firebaseObject($scope.myData.child('profile'));
+    $scope.profile = $firebaseObject($scope.myData);
   
    //creat registration object
     $scope.registration = {}; 
