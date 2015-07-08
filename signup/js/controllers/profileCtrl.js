5cae0182e239443188c4379f11a6c9347e29f6a3
ThomasBangs <BangsSolutions@gmail.com>
2015-07-08 16:15:23 -0500

    fixed profile, added profile info

--- signup/js/controllers/profileCtrl.js
+++ signup/js/controllers/profileCtrl.js
@@ -1,21 +1,18 @@
-app.controller("ProfileCtrl",  function($scope, $rootScope, $location, myService, API_URL, $firebaseAuth, Auth){
+app.controller("ProfileCtrl",  function($scope, $rootScope, $location, myService, API_URL, $firebaseAuth, Auth, $firebaseObject){
 // connect to firebase
 console.log($rootScope);
-    $scope.myData = new Firebase("https://sportssignup.firebaseio.com/users/" + $rootScope.authData.auth.uid + "/profile");
+    $scope.myData = new Firebase("https://sportssignup.firebaseio.com/users/" + $rootScope.authData.auth.uid);
+
+    $scope.profile = $firebaseObject($scope.myData.child('profile'));
   
    //creat registration object
     $scope.registration = {}; 
-    $scope.profiles = {};
+
 
 
   $scope.saveProfile = function(registration) {  
         $scope.myData.set(registration);
         $location.path("/profile");
   };
-  $scope.myData.on("value", function(snapshot) {
-    $scope.profiles = snapshot.val();
-    console.log(snapshot.val());
-    
 
-  })
 });
